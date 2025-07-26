69654456f82ef42691347c23d9a902b48fa0a8e3
Kuro_ai <kmh61030@gmail.com>
2025-07-26 22:06:42 +0630

    fix in monitoring

--- monitoring/views.py
+++ monitoring/views.py
@@ -6,7 +6,7 @@ import time
 def request_count_api(request):
     now = int(time.time())
     #Return the last 60 seconds of request counts
-    counts = [cache.get(f'req_count: (now - i)', 0) for i in range(60)] 
+    counts = [cache.get(f'req_count_{now - i}', 0) for i in range(60)] 
     return JsonResponse({
     "requests_per_second": counts[::-1] # oldest first
     })
